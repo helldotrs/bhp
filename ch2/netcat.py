88185de5ca7d2a255e79b5630ed08fea02276fbb
Rebekah 'hellmak' Hellberg <79146603+hellmak@users.noreply.github.com>
2022-03-02 00:35:23 +0100

    Update netcat.py

--- ch2/netcat.py
+++ ch2/netcat.py
@@ -1,4 +1,5 @@
 import argparse
+from pydoc import describe
 import socket
 import shlex
 import subprocess
@@ -13,3 +14,16 @@ def execute(cmd):
     output = subprocess.check_output(shlex.split(cmd), stderr=subprocess.STDOUT)
 
     return output.decode() 
+
+if __name__ == '__main__':
+    parser  = argparse.ArgumentParser(
+        description = 'BHP Net Tool',
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        epilog=textwarp.dedent('''Example:
+            netcat.py -t 192.168.1.108 -p 5555 -1 -c # command shell
+            netcat.py -t 192.168.1.108 -p 5555 -1 -u=mytest.txt # upload to file
+            netcat.py -t 192.168.1.108 -p 5555 -1 -e=\"cat /etc/passwd\" # execute command
+            echo 'ABC' | ./netcat.py -t 198.168.1.108 -p 135 # echo text to port 135
+            netcat.py -t 192.168.1.108 -p 5555 # connect to server
+        ''')
+    )
