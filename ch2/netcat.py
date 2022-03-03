81dcf43fdd1b2d553f35dca5426f830972f219d3
Rebekah 'hellmak' Hellberg <79146603+hellmak@users.noreply.github.com>
2022-03-03 22:04:44 +0100

    adding ch2.p14

--- ch2/netcat.py
+++ ch2/netcat.py
@@ -16,6 +16,21 @@ def execute(cmd):
 
     return output.decode() 
 
+#pg14 above main block:
+class NetCat:
+    def __init__(self, args, buffer=None):
+        self.args   = args
+        self.buffer = buffer
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    
+    def run(self):
+        if self.args.listen:
+            self.listen()
+        else:
+            self.send()
+
+
 if __name__ == '__main__':
     parser  = argparse.ArgumentParser(
         description = 'BHP Net Tool',
@@ -28,12 +43,12 @@ if __name__ == '__main__':
             netcat.py -t 192.168.1.108 -p 5555 # connect to server
         ''')
     ) 
-parser.add_argument('-c', '--command',  action='store_true',        help='command shell')
-parser.add_argument('-e', '--execute',                              help='execute specified command')
-parser.add_argument('-l', '--listen',   action='store_true',        help='listen')
-parser.add_argument('-p', '--port',     type=int,   default=5555,   help='specifed port')
-parser.add_argument('-t', '--target',   default='192.168.1.203',    help='specified ip')
-parser.add_argument('-u', '--upload',                               help='upload file')
+parser.add_argument('-c', '--command',  action='store_true',        help='command shell'                )
+parser.add_argument('-e', '--execute',                              help='execute specified command'    )
+parser.add_argument('-l', '--listen',   action='store_true',        help='listen'                       )
+parser.add_argument('-p', '--port',     type=int,   default=5555,   help='specifed port'                )
+parser.add_argument('-t', '--target',   default='192.168.1.203',    help='specified ip'                 )
+parser.add_argument('-u', '--upload',                               help='upload file'                  )
 args = parser.parse_args()
 if args.listen:
     buffer = ''
