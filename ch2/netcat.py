35fa0f0220b79ed3c6f44e718c68d5b611f6d495
Rebekah 'hellmak' Hellberg <79146603+hellmak@users.noreply.github.com>
2022-03-07 11:15:51 +0100

    adding code, ch02p015

--- ch2/netcat.py
+++ ch2/netcat.py
@@ -16,6 +16,33 @@ def execute(cmd):
 
     return output.decode() 
 
+def send(self):
+    self.socket.connect((self.args.target, self.args.port))
+    if self.buffer:
+        self.socket.send(self.buffer)
+
+try:
+    while True:
+        recv_len    = 1
+        response    = ''
+        while recv_len:
+            data        =   self.socket.recv(4096)
+            recv_len    =   len(data)
+            response    +=  data.decode()
+            if recv_len < 4096:
+                break
+            if response:
+                print(response)
+                buffer  =   input('> ')
+                buffer  +=  '\n'
+                self.socket.send(buffer.encode())
+    except KeyboardInterrupt:
+        print('User terminated.')
+        self.socket.close()
+        sys.exit()
+
+
+
 #pg14 above main block:
 class NetCat:
     def __init__(self, args, buffer=None):
