77f06693c47d0d073b10d52a9ce2ae44cc1d20b2
Rebekah 'hellmak' Hellberg <79146603+hellmak@users.noreply.github.com>
2022-03-15 14:33:28 +0100

    Fixing indentation error

--- ch2/netcat.py
+++ ch2/netcat.py
@@ -37,29 +37,29 @@ class NetCat:
             self.send()
 
     def send(self):
-    self.socket.connect((self.args.target, self.args.port))
-    if self.buffer:
-        self.socket.send(self.buffer)
+        self.socket.connect((self.args.target, self.args.port))
+        if self.buffer:
+            self.socket.send(self.buffer)
 
-    try:
-        while True:
-            recv_len    = 1
-            response    = ''
-            while recv_len:
-                data        =   self.socket.recv(4096)
-                recv_len    =   len(data)
-                response    +=  data.decode()
-                if recv_len < 4096:
-                    break
-                if response:
-                    print(response)
-                    buffer  =   input('> ')
-                    buffer  +=  '\n'
-                    self.socket.send(buffer.encode())
-        except KeyboardInterrupt:
-            print('User terminated.')
-            self.socket.close()
-            sys.exit()
+        try:
+            while True:
+                recv_len    = 1
+                response    = ''
+                while recv_len:
+                    data        =   self.socket.recv(4096)
+                    recv_len    =   len(data)
+                    response    +=  data.decode()
+                    if recv_len < 4096:
+                        break
+                    if response:
+                        print(response)
+                        buffer  =   input('> ')
+                        buffer  +=  '\n'
+                        self.socket.send(buffer.encode())
+            except KeyboardInterrupt:
+                print('User terminated.')
+                self.socket.close()
+                sys.exit()
     
     def listen(self):
         self.socket.bind((self.args.target, self.args.port))
